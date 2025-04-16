8892e1c59e9bd6ff2f312dd83a76f3bce17c9677
NhonoLama <wadalama111@gmail.com>
2025-04-16 17:02:56 +0545

    project showcase part done

--- src/components/sections/ShowcaseSection.jsx
+++ src/components/sections/ShowcaseSection.jsx
@@ -1,12 +1,77 @@
-import React from "react";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+
+    projects.forEach((project, index) => {
+      gsap.fromTo(
+        project,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: index * 0.2,
+          scrollTrigger: { trigger: project, start: "top bottom-=100" },
+        }
+      );
+    });
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
+  }, []);
+
   return (
-    <div id="work" className="app-showcase">
+    <section ref={sectionRef} id="work" className="app-showcase">
       <div className="w-full">
-        <div className="showcaselayout"></div>
+        <div className="showcaselayout">
+          {/* lEFT */}
+          <div className="first-project-wrapper" ref={project1Ref}>
+            <div className="image-wrapper">
+              <img src="/images/project1.png" alt="Pathao" />
+            </div>
+            <div className="text-content">
+              <h2>ON_demand ride shareing </h2>
+              <p className="text-white-50 md:text-xl">
+                random description on the app thati made ride sharing app
+              </p>
+            </div>
+          </div>
+          {/* RIGHT */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="/images/project2.png" alt="" />
+                <h2>My Project 2</h2>
+              </div>
+            </div>
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[#ffe7eb]">
+                <img src="/images/project3.png" alt="" />
+                <h2>My Project 3</h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
